fd42ee18d40b4c3e5f6c4c822fa4bbe0cccd6068
Rob Whelan <hallwayraptor@gmail.com>
2022-03-21 12:21:16 -0300

    added ui to RPS game

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const rockBtn = document.getElementById(`rockBtn`);
+const paperBtn = document.getElementById(`paperBtn`);
+const scissorsBtn = document.getElementById(`scissorsBtn`);
+const scoreBox = document.querySelector(`#scoreBox`);
+const totalBox = document.querySelector(`#totalBox`);
+const roundBox = document.getElementById(`roundBox`);
+
+let playerChoice = "";
 let symbol = "";
 let playerScore = 0;
 let computerScore = 0;
@@ -6,7 +14,6 @@ let computerScore = 0;
 function getRandomNum(num) {
 	return Math.floor(Math.random() * num);
 }
-
 // assign computer a number from 0-2
 function computerChoice() {
 	choice = getRandomNum(3);
@@ -24,55 +31,64 @@ function computerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 	computerSelection = computerChoice();
-	playerSelection = prompt(`Chose Rock, Paper, or Scissors`).toLowerCase();
+	playerSelection = playerChoice;
 
 	if (
 		(computerSelection === "rock" && playerSelection === "rock") ||
 		(computerSelection === "paper" && playerSelection === "paper") ||
 		(computerSelection === "scissors" && playerSelection === "scissors")
 	) {
-		console.log(
-			`You chose ${playerSelection}. computer chose ${computerSelection}. You tied.`
-		);
+		scoreBox.innerHTML = `You chose ${playerSelection}. Computer chose ${computerSelection}. You tied.`;
 	} else if (
 		(computerSelection === "rock" && playerSelection === "paper") ||
 		(computerSelection === "paper" && playerSelection === "scissors") ||
 		(computerSelection === "scissors" && playerSelection === "rock")
 	) {
-		console.log(
-			`You chose ${playerSelection}. Computer chose ${computerSelection}. You win.`
-		);
+		scoreBox.innerHTML = `You chose ${playerSelection}. Computer chose ${computerSelection}. You win.`;
 		return playerScore++;
 	} else if (
 		(computerSelection === "rock" && playerSelection === "scissors") ||
 		(computerSelection === "paper" && playerSelection === "rock") ||
 		(computerSelection === "scissors" && playerSelection === "paper")
 	) {
-		console.log(
-			`You chose ${playerSelection}. Computer chose ${computerSelection}. You lose.`
-		);
+		scoreBox.innerHTML = `You chose ${playerSelection}. Computer chose ${computerSelection}. You lose.`;
 		return computerScore++;
 	} else {
 		console.log(`something went wrong`);
 	}
 }
 
-function game() {
-	for (let i = 0; i < 5; i++) {
-		playRound();
-		console.log(
-			`Your score is ${playerScore}, opponent's score is ${computerScore}`
-		);
-	}
-
-	console.log(`The score is You: ${playerScore} Computer: ${computerScore}`);
-	if (playerScore > computerScore) {
-		console.log(`You win`);
-	} else if (computerScore > playerScore) {
-		console.log(`You lose`);
-	} else {
-		console.log(`You tied`);
+function displayWinner() {
+	if (playerScore >= 5) {
+		totalBox.textContent = `You win!`;
+		playerScore = 0;
+		computerScore = 0;
+	} else if (computerScore >= 5) {
+		totalBox.textContent = `The computer wins!`;
+		playerScore = 0;
+		computerScore = 0;
 	}
 }
 
-game();
+rockBtn.addEventListener("click", () => {
+	playerChoice = "rock";
+	playRound();
+	totalBox.innerHTML = `Player = ${playerScore} Computer = ${computerScore}`;
+	displayWinner();
+});
+
+paperBtn.addEventListener("click", () => {
+	playerChoice = "paper";
+	playRound();
+	totalBox.innerHTML = `Player = ${playerScore} Computer = ${computerScore}`;
+	displayWinner();
+});
+
+scissorsBtn.addEventListener("click", () => {
+	playerChoice = "scissors";
+	playRound();
+	totalBox.innerHTML = `Player = ${playerScore} Computer = ${computerScore}`;
+	displayWinner();
+});
+
+totalBox.innerHTML = `Player = ${playerScore} Computer = ${computerScore}`;
